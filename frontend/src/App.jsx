934ca342c9537ce180659379a6955d2ae6c95afe
Sanjayazhagan <sanjayazhagan@gmail.com>
2025-08-11 23:55:20 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { FaMicrophone } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <div className='w-2.5 h-2.5 m-20'>
+      <FaMicrophone className="w-10 h-10" />
+    </div>
+  )
+}
+
+export default App
