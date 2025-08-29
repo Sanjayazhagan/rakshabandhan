998c696220221e98bffbc5bafda8aa106524b600
P B Keerthivasan <keerthivasanpb.codesforfun@gmail.com>
2025-08-29 19:02:27 +0530

    add sidebar and homepage

--- frontend/src/components/Questionchatbubble.jsx
+++ frontend/src/components/Questionchatbubble.jsx
@@ -1,7 +1,7 @@
 export default function QuestionBubble({ text }) {
   return (
     <div className="flex justify-end my-15">
-      <div className="bg-blue-500 text-white px-4 py-2 rounded-2xl max-w-full break-words">
+      <div className="bg-blue-500 text-white px-4 py-2 rounded-2xl max-w-3/4 break-words">
         {text}
       </div>
     </div>
